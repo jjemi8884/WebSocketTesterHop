f366a1cfdff4e1305a57f89ee6e852fb9e9bebe4
Justin Jemison <jjemi8884@gmail.com>
2025-02-27 18:39:11 -0800

    got WS working

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const websocket = require('ws');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server); // Initialize socket.io
+const server = new websocket.Server({port:3000});
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/resources/index.html"); // Serve the HTML file
 });
 
-io.on('connection', (socket) => {
+server.on('connection', (ws) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', { from: 'Server', text: 'Welcome!', createdAt: Date.now() });
+    ws.send('This is a test');
 
-    socket.on('createMessage', (message) => { 
-        console.log('New message:', message);
-        startTimmer(message);
-        io.emit('newMessage', message); // Send to everyone
-    });
+    ws.onmessage = (event) => {
+        console.log("Received: ", event.data);
+        ws.send('received message');
 
-    socket.on('disconnect', () => {
+    };
+
+    // ws.on('createMessage', (message) => { 
+    //     console.log('New message:', message);
+    //     ws.send('received message' + message); // Send to everyone
+    // });
+
+    ws.onclose= () => {
         console.log('User disconnected');
-    });
+    };
 });
 
-function startTimmer(message){
-    
-    let i = 10;
-    if(message.text === "start"){
-        console.log("Starting the timer prepare for count down");
-        let i = 1
-        setInterval(() => {
-            message.text = i.toString();
-            io.emit('newMessage', message);
-            i++;
-    },1000,message,i);
-    
-}}
 
-server.listen(port, () => {
+
+server.addListener(port, () => {
     console.log(`Server is up on port ${port}`);
 });
